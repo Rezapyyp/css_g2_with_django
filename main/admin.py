ab81aeadbfac3b2da3ed1f8177283f6701afcc6b
reza <reza.asa@gamil.com>
2024-08-20 15:59:53 +0330

    Files DEVELOPED

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Html , Folder
+from .models import Css , Html , Folder
 
 admin.site.register(Html)
+admin.site.register(Css)
 admin.site.register(Folder)
 
--- main/views.py
+++ main/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render , redirect
 from config.settings import BASE_DIR
 from os import listdir, system , chdir 
 from django.apps import apps
-from .models import Folder
-from .forms import CreateFolderForm
+from .models import Folder , Html , Css
+from .forms import CreateFolderForm , CreateHtmlFileForm ,CreateCssFileForm
 from django.contrib import messages
 from django.views.generic import TemplateView
 
@@ -21,7 +21,6 @@ def list_folders(request):
         "folders" : folders ,
         "create_folder_form" : form ,
     }
-    print(form)
     return render(request,"main/generator/folders/list.html",context)
 
 def delete_folder(request,folder_id):
@@ -43,6 +42,119 @@ def create_folder(request):
     
     messages.error(request, "folder dont... craeted ...")
     return redirect("main:list_folders")
+
+# def rename_folder()
+#    coming soon
+
+
+
+def files_of_folder(request,folder_name):
+    html_files = Html.objects.filter(folder__name=folder_name)
+    css_files = Css.objects.filter(folder__name=folder_name)
+
+    create_html_form = CreateHtmlFileForm
+    create_css_form = CreateCssFileForm
+
+    context = {
+        "html_files" : html_files ,
+        "css_files" : css_files ,
+        "create_html_form" : create_html_form ,
+        "create_css_form" : create_css_form ,
+        "folder_name" : folder_name ,
+    }
+    return render(request,"main/generator/files/list.html",context)
+
+def delete_html_file(request,folder_name,file_id):
+    file = Html.objects.get(id=file_id)
+    folder = Folder.objects.get(name=folder_name)
+    path = folder.PATH / folder.name / file.name
+    system(f"rm -rf {path}")
+    file.delete()
+    messages.success(request,"Html file Deleted successfully")
+    return redirect("main:files_of_folder", folder.name)
+
+def delete_css_file(request,folder_name,file_id):
+    file = Css.objects.get(id=file_id)
+    folder = Folder.objects.get(name=folder_name)
+    path = folder.PATH / folder.name / file.name
+    system(f"rm -rf {path}")
+    file.delete()
+    messages.success(request,"Css file Deleted successfully")
+    return redirect("main:files_of_folder", folder.name)
+
+
+def create_html_file(request,folder_name):
+    form = CreateHtmlFileForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        folder = Folder.objects.get(name=folder_name)
+        html_file = Html.objects.create(name=cd["name"],folder=folder)
+        html_file.save()
+        
+        messages.success(request, "Html file craeted successfully ...")
+        return redirect("main:files_of_folder",folder_name)
+
+    messages.error(request, "Html file dont... craeted ...")
+    return redirect("main:files_of_folder",folder_name)
+
+def create_css_file(request,folder_name):
+    form = CreateCssFileForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        folder = Folder.objects.get(name=folder_name)
+        html_file = Css.objects.create(name=cd["name"],folder=folder)
+        html_file.save()
+        
+        messages.success(request, "Css file craeted successfully ...")
+        return redirect("main:files_of_folder",folder_name)
+
+    messages.error(request, "Css file dont... craeted ...")
+    return redirect("main:files_of_folder",folder_name)
+
+
+# def rename_html_file()
+#    coming soon
+
+# def rename_css_file()
+#    coming soon
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
