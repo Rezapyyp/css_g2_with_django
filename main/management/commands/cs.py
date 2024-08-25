90e61cdf77aa0db2e288a01b85eac24b83520649
reza <reza.asa@gamil.com>
2024-08-25 14:42:33 +0330

    edit cs.py file

--- main/management/commands/cs.py
+++ main/management/commands/cs.py
@@ -1,7 +1,7 @@
 from django.core.management import BaseCommand
 from django.contrib.auth.models import User
 from main.models import Folder , Html
-from os import system
+from os import system , chdir , listdir , remove
 from config.settings import DATABASES
 from django.contrib.auth.models import User
 
@@ -17,10 +17,19 @@ class Command(BaseCommand):
         # step 1
         db_path = DATABASES["default"]["NAME"]
         system(f"rm -rf {db_path}")
+
+        chdir("main/migrations")
+        system("rm -rf __pycache__")
+        for file in listdir() :
+            remove(file)
+        system("touch __init__.py")
+
+        chdir("/home/rezaasa/Desktop/dj_channels/pro")
+
+
         system("python manage.py makemigrations")
         system("python manage.py migrate")
         user = User.objects.create_superuser(username="reza" , email="reza@gmail.com" , password="reza1313" )
         user.save()
-
 
         system("python manage.py runserver")
